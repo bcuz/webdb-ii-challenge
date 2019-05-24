beac5748045ed53e39c7f217ed54b0baa2a38bea
adam <bcuz@users.noreply.github.com>
2019-05-23 23:16:29 -0400

    post request

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', (req, res) => {
+  const zoo = req.body;
+
+  db.insert(zoo)
+    .into('zoos')
+    .then(ids => {
+      res.status(201).json(ids[0]);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Error adding the zoo" });
+    });
+});
 
 const port = 5001;
 server.listen(port, function() {
